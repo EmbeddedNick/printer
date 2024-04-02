05d543060fb1b2c68812b0c9e3d56bc108cb58a8
NickTheTeacher <147639935+EmbeddedNick@users.noreply.github.com>
2024-04-02 19:38:29 +0700

    Add base-abstract class BasePrintes

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+// virtual func
+// pure virtual func
+class BasePrinter
+{
+public:
+    virtual void Print() = 0;
+};
+
+class PrepPrinter : public BasePrinter
+{
+public:
+    void Print() override
+    {
+        std::cout << "Hello, this is Top prep" << std::endl;
+    }
+};
+
+class StudentPrinter : public BasePrinter
+{
+public:
+    void Print() override
+    {
+        std::cout << "Hello, this is Top student" << std::endl;
+    }
+};
+class AdminPrinter : public BasePrinter
+{
+public:
+    void Print() override
+    {
+        std::cout << "Hello, this is Top adminnistrator" << std::endl;
+    }
+};
+int main()
+{
+    std::vector<BasePrinter*> units;
+    int userChoice;
+    bool bRun = true;
+    while(bRun)
+    {
+        std::cout << "Do you want to add a prep (1) or student (2) or exit (0) or print all units (3) or administrator (4)?"<<std::endl;
+        std::cin >> userChoice;
+
+        switch (userChoice)
+        {
+        case 0: // exit
+            bRun = false;
+            break;
+        case 1:
+            units.push_back(new PrepPrinter());
+            break;
+        case 2:
+            units.push_back(new StudentPrinter());
+            break;
+        case 3:
+            for(auto it = units.begin(); it != units.end(); ++it)
+            {
+                (*it)->Print();
+            }
+            break;
+        case 4:
+            units.push_back(new AdminPrinter());
+            break;
+        default:
+            break;
+        }
+    }
+
+}
